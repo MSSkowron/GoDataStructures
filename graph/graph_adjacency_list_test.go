ab86dffa7811863ed21e08c89f39eb356c47352f
MSSkowron <mateusz.skowron@poczta.onet.pl>
2023-07-25 12:02:00 +0200

    Add tests for graphs

---
+++ graph/graph_adjacency_list_test.go
@@ -0,0 +1,50 @@
+package graph
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewGraphAdjList(t *testing.T) {
+	n := NewGraphAdjList()
+
+	assert.NotNil(t, n)
+	assert.Equal(t, 0, len(n.vertices))
+}
+
+func TestGraphAdjList_AddVertex(t *testing.T) {
+	n := NewGraphAdjList()
+
+	assert.Equal(t, 0, n.AddVertex())
+	assert.Equal(t, 1, n.AddVertex())
+	assert.Equal(t, 2, n.AddVertex())
+	assert.Equal(t, 3, n.AddVertex())
+	assert.Equal(t, 4, n.AddVertex())
+
+	assert.Equal(t, 5, len(n.vertices))
+
+	for i := 0; i < 5; i++ {
+		assert.Equal(t, 0, len(n.vertices[i]))
+	}
+}
+
+func TestGraphAdjList_AddEdge(t *testing.T) {
+	n := NewGraphAdjList()
+
+	assert.Equal(t, ErrIndexOutOfRange, n.AddEdge(0, 1))
+
+	for i := 0; i < 5; i++ {
+		n.AddVertex()
+	}
+
+	assert.Equal(t, ErrIndexOutOfRange, n.AddEdge(0, 5))
+
+	assert.Nil(t, n.AddEdge(0, 1))
+	assert.Equal(t, 1, len(n.vertices[0]))
+	assert.Equal(t, 1, n.vertices[0][0])
+
+	assert.Nil(t, n.AddEdge(0, 2))
+	assert.Equal(t, 2, len(n.vertices[0]))
+	assert.Equal(t, 2, n.vertices[0][1])
+}
---
+++ graph/graph_adjacency_matrix_test.go
@@ -0,0 +1,52 @@
+package graph
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewGraphAdjMatrix(t *testing.T) {
+	n := NewGraphAdjMatrix()
+
+	assert.NotNil(t, n)
+	assert.Equal(t, 0, len(n.vertices))
+}
+
+func TestGraphAdjMatrix_AddVertex(t *testing.T) {
+	n := NewGraphAdjMatrix()
+
+	assert.Equal(t, 0, n.AddVertex())
+	assert.Equal(t, 1, n.AddVertex())
+	assert.Equal(t, 2, n.AddVertex())
+	assert.Equal(t, 3, n.AddVertex())
+	assert.Equal(t, 4, n.AddVertex())
+
+	assert.Equal(t, 5, len(n.vertices))
+
+	for i := 0; i < 5; i++ {
+		assert.Equal(t, 5, len(n.vertices[i]))
+	}
+}
+
+func TestGraphAdjMatrix_AddEdge(t *testing.T) {
+	n := NewGraphAdjMatrix()
+
+	assert.Equal(t, ErrIndexOutOfRange, n.AddEdge(0, 1))
+
+	for i := 0; i < 5; i++ {
+		n.AddVertex()
+	}
+
+	assert.Equal(t, ErrIndexOutOfRange, n.AddEdge(0, 5))
+
+	assert.Nil(t, n.AddEdge(0, 1))
+	assert.True(t, n.vertices[0][1])
+	assert.False(t, n.vertices[1][0])
+
+	assert.Nil(t, n.AddEdge(0, 2))
+	assert.True(t, n.vertices[0][2])
+	assert.False(t, n.vertices[2][0])
+
+	assert.False(t, n.vertices[1][2])
+}
