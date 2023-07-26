0bfaefc0da3005ced30d4d43b6afc46c00537da8
MSSkowron <mateusz.skowron@poczta.onet.pl>
2023-07-26 18:08:46 +0200

    Implement Set and Set's tests

--- set/set.go
+++ set/set.go
@@ -1,24 +1,32 @@
 package set
 
-type Set[T comparable] map[T]struct{}
+type Set[T comparable] struct {
+	data map[T]struct{}
+}
 
 func New[T comparable](values ...T) *Set[T] {
-	set := make(Set[T], 0)
-	for _, v := range values {
-		set[v] = struct{}{}
+	set := &Set[T]{
+		data: make(map[T]struct{}),
 	}
-	return &set
+	for _, v := range values {
+		set.data[v] = struct{}{}
+	}
+	return set
 }
 
-func (s Set[T]) Insert(value T) {
-	s[value] = struct{}{}
+func (s *Set[T]) Insert(value T) {
+	s.data[value] = struct{}{}
 }
 
-func (s Set[T]) Delete(value T) {
-	delete(s, value)
+func (s *Set[T]) Delete(value T) {
+	delete(s.data, value)
 }
 
-func (s Set[T]) Has(value T) bool {
-	_, ok := s[value]
+func (s *Set[T]) Has(value T) bool {
+	_, ok := s.data[value]
 	return ok
+}
+
+func (s *Set[T]) Size() int {
+	return len(s.data)
 }
--- set/set_test.go
+++ set/set_test.go
@@ -1,1 +1,84 @@
 package set
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+	s := New[int]()
+	assert.NotNil(t, s)
+	assert.NotNil(t, s.data)
+	assert.Equal(t, 0, len(s.data))
+}
+
+func TestInsert(t *testing.T) {
+	s := New[int]()
+
+	s.Insert(10)
+	assert.NotNil(t, s.data)
+	assert.Equal(t, 1, len(s.data))
+	_, ok := s.data[10]
+	assert.True(t, ok)
+
+	s.Insert(10)
+	assert.Equal(t, 1, len(s.data))
+	_, ok = s.data[10]
+	assert.True(t, ok)
+
+	s.Insert(20)
+	assert.Equal(t, 2, len(s.data))
+	_, ok = s.data[20]
+	assert.True(t, ok)
+}
+
+func TestDelete(t *testing.T) {
+	s := New[int]()
+
+	s.Insert(10)
+	s.Insert(20)
+	s.Insert(30)
+
+	s.Delete(10)
+	_, ok := s.data[10]
+	assert.False(t, ok)
+
+	s.Delete(20)
+	_, ok = s.data[20]
+	assert.False(t, ok)
+
+	s.Delete(30)
+	_, ok = s.data[30]
+	assert.False(t, ok)
+
+	assert.Equal(t, 0, len(s.data))
+}
+
+func TestHas(t *testing.T) {
+	s := New[int]()
+
+	s.Insert(10)
+	s.Insert(20)
+	s.Insert(30)
+
+	assert.True(t, s.Has(10))
+	assert.True(t, s.Has(20))
+	assert.True(t, s.Has(30))
+	assert.False(t, s.Has(50))
+}
+
+func TestSize(t *testing.T) {
+	s := New[int]()
+	assert.Equal(t, 0, s.Size())
+
+	for i := 1; i <= 5; i++ {
+		s.Insert(i)
+		assert.Equal(t, i, s.Size())
+	}
+
+	for i := 4; i <= 0; i++ {
+		s.Delete(i)
+		assert.Equal(t, i, s.Size())
+	}
+}
