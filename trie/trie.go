31e2fcc5166c2ad00805c80ca8497b0d2202618b
MSSkowron <mateusz.skowron@poczta.onet.pl>
2023-07-26 21:07:18 +0200

    Implement Trie & tests

--- trie/trie.go
+++ trie/trie.go
@@ -1,4 +1,62 @@
 package trie
 
+const AlphabetSize = 26
+
+type trieNode struct {
+	children [AlphabetSize]*trieNode
+	isEnd    bool
+}
+
 type Trie struct {
+	root *trieNode
+}
+
+func New() *Trie {
+	return &Trie{
+		root: &trieNode{},
+	}
+}
+
+func (t *Trie) Insert(word string) {
+	curr := t.root
+	for _, char := range word {
+		charIndex := char - 'a'
+		if curr.children[charIndex] == nil {
+			curr.children[charIndex] = &trieNode{}
+		}
+		curr = curr.children[charIndex]
+	}
+
+	curr.isEnd = true
+}
+
+func (t *Trie) Delete(word string) {
+	t.delete(t.root, word, 0)
+}
+
+func (t *Trie) delete(curr *trieNode, word string, index int) {
+	if index == len(word) {
+		if curr.isEnd {
+			curr.isEnd = false
+		}
+		return
+	}
+
+	charIndex := rune(word[index]) - 'a'
+	if nextNode := curr.children[charIndex]; nextNode != nil {
+		t.delete(nextNode, word, index+1)
+	}
+}
+
+func (t *Trie) Search(word string) bool {
+	curr := t.root
+	for _, char := range word {
+		charIndex := char - 'a'
+		if curr.children[charIndex] == nil {
+			return false
+		}
+		curr = curr.children[charIndex]
+	}
+
+	return curr.isEnd
 }
--- trie/trie_test.go
+++ trie/trie_test.go
@@ -1,1 +1,52 @@
 package trie
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+	trie := New()
+
+	assert.NotNil(t, trie)
+	assert.NotNil(t, trie.root)
+	assert.False(t, trie.root.isEnd)
+
+	for _, child := range trie.root.children {
+		assert.Nil(t, child)
+	}
+}
+
+func TestInsert(t *testing.T) {
+	trie := New()
+
+	trie.Insert("abc")
+	assert.True(t, trie.root.children[0].children[1].children[2].isEnd)
+
+	trie.Insert("")
+	assert.True(t, trie.root.isEnd)
+}
+
+func TestDelete(t *testing.T) {
+	trie := New()
+	trie.Insert("abc")
+
+	trie.Delete("abc")
+	assert.False(t, trie.Search("abc"))
+
+	trie.Delete("xyz")
+	assert.False(t, trie.Search("xyz"))
+}
+
+func TestSearch(t *testing.T) {
+	trie := New()
+	trie.Insert("abc")
+
+	assert.True(t, trie.Search("abc"))
+	assert.False(t, trie.Search("a"))
+	assert.False(t, trie.Search("ab"))
+	assert.False(t, trie.Search("abcd"))
+	assert.False(t, trie.Search("xyz"))
+	assert.False(t, trie.Search(""))
+}
