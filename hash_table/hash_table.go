2410f29c128a382d2bfbd807a41acd745e083670
MSSkowron <mateusz.skowron@poczta.onet.pl>
2023-07-26 18:55:31 +0200

    Make hash table generic

--- hash_table/hash_table.go
+++ hash_table/hash_table.go
@@ -5,47 +5,54 @@ import sll "github.com/MSSkowron/GoDataStructuresAndAlgorithms/linked_list/singl
 // ArrSize is the size of the hash table array
 const ArrSize = 100
 
-// HashTable is a data structure that stores key-value pairs.
-// The hash table is implemented using separate chaining.
-type HashTable struct {
-	arr [ArrSize]*sll.LinkedList[string]
+// HashTable is a data structure that stores values based on theirs hash value.
+// HashTable can be used with either int or string type.
+type HashTable[T int | string] struct {
+	arr [ArrSize]*sll.LinkedList[T]
 }
 
 // New creates a new hash table
-func New() *HashTable {
-	ht := &HashTable{}
+func New[T int | string]() *HashTable[T] {
+	ht := &HashTable[T]{}
 	ht.init()
 	return ht
 }
 
-func (ht *HashTable) init() {
+func (ht *HashTable[T]) init() {
 	for i := range ht.arr {
-		ht.arr[i] = sll.New[string]()
+		ht.arr[i] = sll.New[T]()
 	}
 }
 
-// Insert inserts a new key-value pair into the hash table
-func (ht *HashTable) Insert(key string) {
-	index := hash(key)
-	ht.arr[index].Prepend(key)
+// Insert inserts a new value to the hash table
+func (ht *HashTable[T]) Insert(value T) {
+	index := hash(value)
+	ht.arr[index].Prepend(value)
 }
 
-// Delete deletes a key-value pair from the hash table
-func (ht *HashTable) Delete(key string) {
-	index := hash(key)
-	ht.arr[index].Delete(key)
+// Delete deletes a value from the hash table
+func (ht *HashTable[T]) Delete(value T) {
+	index := hash(value)
+	ht.arr[index].Delete(value)
 }
 
-// Search searches for a key in the hash table
-func (ht *HashTable) Search(key string) bool {
-	index := hash(key)
-	return ht.arr[index].Contains(key)
+// Search searches for a value in the hash table
+func (ht *HashTable[T]) Search(value T) bool {
+	index := hash(value)
+	return ht.arr[index].Contains(value)
 }
 
-func hash(key string) int {
-	var sum int
-	for _, v := range key {
-		sum += int(v)
+func hash[T int | string](value T) int {
+	switch any(value).(type) {
+	case string:
+		var sum int
+		for _, v := range any(value).(string) {
+			sum += int(v)
+		}
+		return sum % ArrSize
+	case int:
+		return any(value).(int) % ArrSize
+	default:
+		return -1
 	}
-	return sum % ArrSize
 }
--- hash_table/hash_table_test.go
+++ hash_table/hash_table_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestNew(t *testing.T) {
-	ht := New()
+	ht := New[string]()
 	assert.NotNil(t, ht)
 	assert.Equal(t, ArrSize, len(ht.arr))
 
@@ -18,41 +18,75 @@ func TestNew(t *testing.T) {
 }
 
 func TestInsert(t *testing.T) {
-	ht := New()
+	htString := New[string]()
 
-	ht.Insert("foo")
+	htString.Insert("foo")
 	idx := hash("foo")
-	assert.Equal(t, 1, ht.arr[idx].Length())
-	assert.True(t, ht.arr[idx].Contains("foo"))
+	assert.Equal(t, 1, htString.arr[idx].Length())
+	assert.True(t, htString.arr[idx].Contains("foo"))
 
-	ht.Insert("bar")
+	htString.Insert("bar")
 	idx = hash("bar")
-	assert.Equal(t, 1, ht.arr[idx].Length())
-	assert.True(t, ht.arr[idx].Contains("bar"))
+	assert.Equal(t, 1, htString.arr[idx].Length())
+	assert.True(t, htString.arr[idx].Contains("bar"))
+
+	htInt := New[int]()
+
+	htInt.Insert(12)
+	idx = hash(12)
+	assert.Equal(t, 1, htInt.arr[idx].Length())
+	assert.True(t, htInt.arr[idx].Contains(12))
+
+	htInt.Insert(156)
+	idx = hash(156)
+	assert.Equal(t, 1, htInt.arr[idx].Length())
+	assert.True(t, htInt.arr[idx].Contains(156))
 }
 
 func TestDelete(t *testing.T) {
-	ht := New()
-	ht.Insert("foo")
-	ht.Insert("bar")
+	htString := New[string]()
+	htString.Insert("foo")
+	htString.Insert("bar")
 
-	ht.Delete("foo")
+	htString.Delete("foo")
 	idx := hash("foo")
-	assert.Equal(t, 0, ht.arr[idx].Length())
-	assert.False(t, ht.arr[idx].Contains("foo"))
+	assert.Equal(t, 0, htString.arr[idx].Length())
+	assert.False(t, htString.arr[idx].Contains("foo"))
 
-	ht.Delete("bar")
+	htString.Delete("bar")
 	idx = hash("bar")
-	assert.Equal(t, 0, ht.arr[idx].Length())
-	assert.False(t, ht.arr[idx].Contains("bar"))
+	assert.Equal(t, 0, htString.arr[idx].Length())
+	assert.False(t, htString.arr[idx].Contains("bar"))
+
+	htInt := New[int]()
+	htInt.Insert(12)
+	htInt.Insert(156)
+
+	htInt.Delete(12)
+	idx = hash(12)
+	assert.Equal(t, 0, htInt.arr[idx].Length())
+	assert.False(t, htInt.arr[idx].Contains(12))
+
+	htInt.Delete(156)
+	idx = hash(156)
+	assert.Equal(t, 0, htInt.arr[idx].Length())
+	assert.False(t, htInt.arr[idx].Contains(156))
 }
 
 func TestSearch(t *testing.T) {
-	ht := New()
-	ht.Insert("foo")
-	ht.Insert("bar")
+	htString := New[string]()
+	htString.Insert("foo")
+	htString.Insert("bar")
 
-	assert.True(t, ht.Search("foo"))
-	assert.True(t, ht.Search("bar"))
-	assert.False(t, ht.Search("baz"))
+	assert.True(t, htString.Search("foo"))
+	assert.True(t, htString.Search("bar"))
+	assert.False(t, htString.Search("baz"))
+
+	htInt := New[int]()
+	htInt.Insert(12)
+	htInt.Insert(156)
+
+	assert.True(t, htInt.Search(12))
+	assert.True(t, htInt.Search(156))
+	assert.False(t, htInt.Search(243))
 }
