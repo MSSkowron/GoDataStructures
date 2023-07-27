2d1e94ae394aa9f3432d7d43fdd7863c23af7190
MSSkowron <mateusz.skowron@poczta.onet.pl>
2023-07-27 18:20:31 +0200

    Implement ternary search and test

---
+++ a_search/ternary_search/ternary_search.go
@@ -0,0 +1,34 @@
+package ternarysearch
+
+import "golang.org/x/exp/constraints"
+
+type ComparableOrdered interface {
+	comparable
+	constraints.Ordered
+}
+
+func Search[T ComparableOrdered](items []T, value T) int {
+	left, right := 0, len(items)-1
+
+	for left <= right {
+		mid1, mid2 := left+(right-left)/3, right-(right-left)/3
+
+		if items[mid1] == value {
+			return mid1
+		}
+
+		if items[mid2] == value {
+			return mid2
+		}
+
+		if value < items[mid1] {
+			right = mid1 - 1
+		} else if value > items[mid2] {
+			left = mid2 + 1
+		} else {
+			left, right = mid1+1, mid2-1
+		}
+	}
+
+	return -1
+}
